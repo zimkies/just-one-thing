504e3609dbd537b583aac4e2f25fe1761e4d474a
Benjamin Kies <ben@joingrouper.com>
2015-10-27 17:57:38 -0400

    Process Async stuff

--- lib/clockwork.rb
+++ lib/clockwork.rb
@@ -12,5 +12,6 @@ module Clockwork
     puts "Running #{job}, at #{time}"
   end
 
-  every(1.hour, 'task_reminder_worker') { TaskReminderWorker.perform_async }
+  # Perform async once we have more dynos
+  every(1.hour, 'task_reminder_worker') { TaskReminderWorker.new.perform }
 end
